b5ba77ad13b9d874a99a79e3fded14506f5764ed
k <wanlinkun9527@gmail.com>
2023-03-06 01:41:32 +0000

    fix: 合并代码

---
+++ src/utils/StackArray.ts
@@ -0,0 +1,91 @@
+interface Obj {
+    [index: number]: any
+}
+
+class StackArray {
+    private items: Array<any>;
+    constructor() {
+        // 创建一个数组用于保存栈
+        this.items = [];
+    };
+
+    // 添加元素到栈顶
+    push(element: any) {
+        this.items.push(element);
+    };
+
+    // 移除栈顶元素, 并返回值
+    pop() {
+        return this.items.pop()
+    };
+
+    // 返回栈顶元素, 不做任何操作
+    peek() {
+        return this.items[this.items.length - 1]
+    };
+
+    // 判断栈中是否有元素, 如果没有，就返回 true 否则返回 false
+    isEmpty() {
+        return this.items.length === 0
+    };
+
+    // 清空栈中元素
+    clear() {
+        this.items = []
+    };
+
+    // 反回栈的长度
+    size() {
+        return this.items.length;
+    };
+}
+
+class StackObject {
+    private items: Obj;
+    private count: number;
+
+    constructor() {
+        // 保存栈
+        this.items = {};
+        // 记录栈的长度
+        this.count = 0;
+    };
+
+    isEmpty() {
+        return this.count === 0
+    }
+
+    push(element: any) {
+        this.items[this.count++] = element;
+    }
+
+    pop() {
+        if(this.isEmpty()) {
+            return undefined;
+        }
+        const result = this.items[--this.count]
+        delete this.items[this.count]
+
+        return result;
+    }
+
+    peek() {
+        if(this.isEmpty()) {
+            return undefined
+        }
+
+        return this.items[this.count - 1]
+    }
+
+    clear() {
+        this.items = {};
+        this.count = 0;
+    }
+
+    size() {
+        return this.count
+    }
+}
+
+export const stack = new StackArray();
+export const stackObj = new StackObject();
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <el-button v-for="item in list" :key="item.id" @click="traversal">{{ item.name }}</el-button>
   </div>
 </template>
 
 <script lang="ts">
+<<<<<<< HEAD
 import { defineComponent } from "vue";
+=======
+import { defineComponent, reactive } from "vue"
+
+import {stack, stackObj} from '../utils/StackArray.js';
+>>>>>>> 76f06d1 (feat: 数据结构练习-栈)
 
 export default defineComponent({
   name: "AboutComponent",
@@ -13,8 +19,66 @@ export default defineComponent({
     // const { attrs, slots, emit, expose } = ctx
     console.log("ctx", ctx);
     console.log("props", props);
+<<<<<<< HEAD
   },
 });
+=======
+    const list = reactive([
+      {
+        id: 1000,
+        name: 1,
+      },{
+        id: 2000,
+        name: 2
+      },{
+        id: 3000,
+        name: 3
+      }, {
+        id: 4000,
+        name: 4
+      },
+      {
+        id: 2000,
+        name: 5
+      }
+    ])
+    
+    stackObj.push(1)
+    stackObj.push(2)
+    stackObj.push(3)
+    stackObj.push(4)
+    
+
+    stack.push(1);
+    stack.size();
+
+    console.log(stack.size());
+
+    console.log(stack);
+    console.log(stackObj);
+
+    console.log(stackObj.peek())
+    console.log(stackObj.pop())
+    console.log(stackObj.size())
+    
+
+    function timeConsuming(inputVal: number, time = 0){
+      const timerId = setTimeout(() => {
+        console.log(inputVal)
+      }, time)
+    }
+    
+    function traversal(){
+      for(let i = 0; i < 5; i++){
+        let timer = i === 4 ? 2500 : i * 1000;
+        timeConsuming(i, timer)
+      }
+    }
+
+    return {list, traversal}
+  }
+})
+>>>>>>> 76f06d1 (feat: 数据结构练习-栈)
 </script>
 
 <style>
